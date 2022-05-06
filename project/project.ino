5a37e57f5e7d6c45fa4e32e7779af9167a9db475
Akshat412 <akshat412000@gmail.com>
2022-05-06 13:50:18 -0400

    Removed serial, moved things around

--- project/project.ino
+++ project/project.ino
@@ -12,33 +12,44 @@
 #include "SparkFun_Si7021_Breakout_Library.h"
 #include <Adafruit_SSD1306.h>
 
+/* define pins for hook-up */
+#define LED     PD6
+#define BUTTON  PD2
+
+#define BEEP    A0 
+
+/* define range for temperature and humidity */
+#define TEMP_HIGH 25
+#define TEMP_LOW  15
+
+#define HUM_HIGH  50
+#define HUM_LOW   30 
+
 // object for SSD1306 display
 Adafruit_SSD1306 display(128,64, &Wire, 4); 
 
 // object for Si7021 sensor 
 Weather sensor;
 
-/* define pins for hook-up */
-#define LED     PD6
-#define BUTTON  PD2
-
 /* define variables */
 float humidity = 0;
 float tempC = 0;
 
-bool goToSleep = 0;   // if 1, don't sleep
-int delayTime = 10;   // time to delay OLED
+bool goToSleep = false;       // if 1, don't sleep
+bool updateDisplay = false;   // if 1, update display
 
 void setup() {
   // set button as INPUT_PULLUP
   DDRD  &= ~(1 << BUTTON);  
   PORTD |= (1 << BUTTON);
-  
-  Serial.begin(9600); // initialize serial module 
-  sensor.begin();     // initialize sensor 
 
-  // set all pins other than TX, RX and INT0 to OUTPUT, reduces power 
-  for(int i = 3; i < 20; i++) pinMode(i, OUTPUT); 
+  // set LED as OUTPUT
+  DDRD |= (1 << LED);
+
+  // set BEEP as OUTPUT
+  pinMode(BEEP, OUTPUT);
+
+  sensor.begin(); // initialize sensor 
 
   // enable sleep and power-down mode 
   SMCR |= (1 << SM1);   // power-down mode
@@ -56,27 +67,50 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Main loop"); 
-  
+  // sample sensor values from Si7021
   humidity = sensor.getRH();
   tempC = sensor.getTemp();
 
-  display.setCursor(1, 1);
-  display.print("Temperature: ");
-  display.print(tempC);
-  display.println("C");
-  display.print("Humidity: ");
-  display.print(humidity);
-  display.println("%");
-  display.display();
+  // compare temperature and humidity ratings 
+  if((tempC < TEMP_LOW || tempC > TEMP_HIGH) || (humidity < HUM_LOW || humidity > HUM_HIGH)) {
+    // warn user about their surroundings 
+    PORTD |= (1 << LED);
 
-  delay(delayTime);
+    tone(BEEP, 1000);
+    delay(500);
+    noTone(BEEP); 
+  }
 
-  display.clearDisplay();
-  display.display(); 
+  else {
+    // surroundings are comfortable 
+    PORTD &= ~(1 << LED);
+  }
 
-  delayTime = 10; 
-  powerDownWatchdog(4); 
+  // woken up by external interrupt, activate OLED
+  if(updateDisplay == true) {
+    // write temperature and humidity to sensor 
+    display.setCursor(1, 1);
+    
+    display.print("Temperature: ");
+    display.print(tempC);
+    display.println("C");
+    
+    display.print("Humidity: ");
+    display.print(humidity);
+    display.println("%");
+    display.display();
+
+    // wait for 5 seconds 
+    delay(5000);
+
+    // clear display 
+    display.clearDisplay();
+    display.display(); 
+  } 
+
+  // disable OLED activation and sleep for 10 seconds 
+  updateDisplay = false; 
+  powerDownWatchdog(60); 
 }
 
 /*
@@ -84,11 +118,12 @@ void loop() {
  * ---------------------------
  *  Puts device to sleep and triggers wake-up using watchdog interrupt   
  *  
- *  sleep_time: amount of time the device sleeps for
+ *  sleep_time: amount of time the device sleeps for, in seconds 
  *  
  *  returns: none
  */
 void powerDownWatchdog(int sleep_time) {
+  // initialize watchdog timer 
   WDTCSR |= (1 << WDCE) | (1 << WDE);               // set watchdog enable (WDE) and watchdog change enable (WDCE)
   WDTCSR = (1 << WDP0)| (1 << WDP1)| (1 << WDP2);   // prescaler, set for 2s counter, get rid of WDE and WDCE
   WDTCSR |= (1 << WDIE);                            // set interrupt enable
@@ -97,15 +132,16 @@ void powerDownWatchdog(int sleep_time) {
   
   delay(10); // add minor delay to make sure any previous code executes
   
-  // 1 minute of sleep time, 60 = 4 * 15, where 15 is the number of loops
+  // device sleeps for sleep_time seconds 
   for(unsigned char i = 0; i < sleep_time; i++) {
-    if(goToSleep == 1) break; 
+    // if woken up by external interrupt, break out of loop 
+    if(goToSleep == true) break; 
     asm("sleep"); // inline assembler to go to sleep
   }
   
   WDTCSR |= (1 << WDE) | (1 << WDCE); // set watchdog enable (WDE) and watchdog change enable (WDCE)
   WDTCSR = 0x00;                      // turn off WDT
-  goToSleep = 0;  
+  goToSleep = false;  
 }
 
 /*
@@ -130,8 +166,6 @@ ISR(WDT_vect) {
  */
 void digitalInterrupt() {
   // digital button-controlled interrupt
-  goToSleep = 1; 
-  delayTime = 5000; 
-  
-  Serial.println("Digital interrupt!");  
+  goToSleep = true; 
+  updateDisplay = true;  
 }
